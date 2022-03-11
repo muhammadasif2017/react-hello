007d6b2a8d33c0e073e409d6937ed2fa727188ec
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-03-12 00:30:58 +0500

    hello world initial react program

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// function Hello() {
+//   return <span>Hello</span>
+// }
+
+// function World() {
+//   return <span>World</span>
+// }
+
+// function HelloWorld() {
+//   return (
+//     <div>
+//       <Hello /> <World />
+//     </div>
+//   );
+// }
+// function NameCells() {
+//   return (
+//     <React.Fragment>
+//       <td>First Name</td>
+//       <td>Last Name</td>
+//     </React.Fragment>
+//   );
+// }
+
+// function ValidIndicator() {
+//   const isValid = false;
+//   return (
+//     <span>{ isValid ? 'valid' : 'not valid'}</span>
+//   )
+// }
+
+function ValidIndicator() {
+  const isValid = false;
+  return (
+    <span>
+      {/* here is a comment */}
+      { isValid && 'valid'}
+      { !isValid && 'not valid'}
+    </span>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <ValidIndicator />, document.querySelector("#root")
+  );
